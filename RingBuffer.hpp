50b6d274bb836e24d7ac9fe7f9b65cd6acfea14f
Eshan Chatterjee <eshan2001221@gmail.com>
2020-08-20 00:24:56 +0530

    first commit

---
+++ RingBuffer.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+template <typename T>
+class ring
+{
+    private:
+
+
+    public:
+        class iterator;
+
+    public:
+};
+
+
+
+template<typename T>
+class iterators
+{
+
+    
+};
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "RingBuffer.hpp"
+
+
+int main()
+{
+    std::cout << "Hello World!!" << std::endl;
+    ring<std::string> text;
+    text.add("One");
+    text.add("Two");
+    text.add("Three");
+
+    //Iterative loop
+    for (int i = 0; i < text.size(); ++i)
+    {
+        std::cout << text.get(i) << std::endl;
+    }
+}
